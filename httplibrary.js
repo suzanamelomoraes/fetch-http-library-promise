45ce941872eb74ef5fdea92357a7a381ae08a75f
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-29 15:07:46 +1300

    create http request to update data

--- httplibrary.js
+++ httplibrary.js
@@ -29,4 +29,20 @@ class EasyHTTP {
         .catch(err => reject(err));
     });
   }
+
+  // Make an HTTP PUT request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
 }
