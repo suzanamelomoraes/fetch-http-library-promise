79cd0f3f979eaaecf2fbca2c78ffe1c41446eca6
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-29 14:58:11 +1300

    create HTTP request to post data

--- httplibrary.js
+++ httplibrary.js
@@ -13,4 +13,20 @@ class EasyHTTP {
         .catch(err => reject(err));
     });
   }
+
+  // Make an HTTP POST request
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
 }
